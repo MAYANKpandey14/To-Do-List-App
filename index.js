415c1b5bcef656f3e8265b6a1d01e89d9d2f2972
MAYANKpandey14 <mayankpandeyofficial404@gmail.com>
2024-06-09 11:43:30 +0530

    Optimize js

--- index.js
+++ index.js
@@ -1,135 +1,19 @@
-
-//** this code has the issue to trigger alert only when no task is added afterwards it adds blank tasks. */
-
-// const inputBox = document.getElementById("input-box");
-// const listContainer= document.getElementById("list-container");
-// let check = false;
-
-// function addTask(){
-//     if(inputBox.value.length == 0){
-//         alert("Kindly Enter Task Name!!!!")
-//     }
-
-//     else{
-//         let li = document.createElement("li");
-//         li.innerHTML = inputBox.value;
-//         listContainer.appendChild(li);
-//         let span = document.createElement("span");
-//         span.innerHTML= "\u00D7";
-//         li.appendChild(span);
-//         check=true;
-//     }
-//     if(check !== false){
-//     inputBox.value=" ";
-//     }
-//     saveData();
-
-
-// }
-
-// listContainer.addEventListener("click", function(r){
-//     if(r.target.tagName==="LI"){
-//         r.target.classList.toggle("checked");
-//         saveData();
-//     }
-//     else if(r.target.tagName==="SPAN"){
-//         r.target.parentElement.remove();
-//         check=false;
-//         saveData();
-
-//     }
-// }, false);
-
-// function saveData(){
-//     localStorage.setItem("data",listContainer.innerHTML);
-// }
-
-// function showTask(){
-//     listContainer.innerHTML=localStorage.getItem("data");
-// }
-
-// showTask();
-
-//-------------------------------------------------------------
-
-// * new js code 
-// ** --This has error of adding a task even if input is only whitespace.
-
-// "use strict";
-
-// const inputBox = document.querySelector("#input-box");
-// const listContainer = document.querySelector("#list-container");
-// let check = false;
-
-// function addTask() {
-//   if (inputBox.value.length === 0) {
-//     alert("Kindly Enter Task Name!!!!");
-//   } else {
-//     const li = document.createElement("li");
-//     li.textContent = inputBox.value;
-//     listContainer.appendChild(li);
-
-//     const span = document.createElement("span");
-//     span.innerHTML = "\u00D7";
-//     li.appendChild(span);
-
-//     check = true;
-//   }
-
-//   if (check !== false) {
-//     inputBox.value = "";
-//   }
-
-//   saveData();
-// }
-
-// listContainer.addEventListener("click", function (event) {
-//   const target = event.target;
-
-//   if (target.tagName === "LI") {
-//     target.classList.toggle("checked");
-//     saveData();
-//   } else if (target.tagName === "SPAN") {
-//     target.parentElement.remove();
-//     check = false;
-//     saveData();
-//   }
-// });
-
-// function saveData() {
-//   localStorage.setItem("data", listContainer.innerHTML);
-// }
-
-// function showTask() {
-//   listContainer.innerHTML = localStorage.getItem("data") || "";
-// }
-
-// showTask();
-
-//-------------------------------------------------------------------------------
-
-// * 3rd version
-
 'use strict';
 
 const inputBox = document.querySelector('#input-box');
 const listContainer = document.querySelector('#list-container');
+const addButton = document.querySelector('#add-button'); // Assuming you have a button with this id
 const checkedClass = 'checked';
 
-document.querySelector('#input-box').addEventListener('keypress', function (e) {
-    if (e.key === 'Enter') {
-      addTask();
-    }
-});
-
 function addTask() {
-  if (!inputBox.value.trim()) {
+  const taskName = inputBox.value.trim();
+  if (!taskName) {
     alert('Please enter a task name!');
     return;
   }
 
   const li = document.createElement('li');
-  li.textContent = inputBox.value.trim();
+  li.textContent = taskName;
   listContainer.appendChild(li);
 
   const span = document.createElement('span');
@@ -139,6 +23,16 @@ function addTask() {
   inputBox.value = '';
   saveData();
 }
+
+inputBox.addEventListener('keypress', function (e) {
+  if (e.key === 'Enter') {
+    addTask();
+  }
+});
+
+addButton.addEventListener('click', function () {
+  addTask();
+});
 
 listContainer.addEventListener('click', event => {
   const target = event.target;
